5edb0dd9a97515b67de385d22bc9b06515dfe5e3
cristianml29 <156484062+cristianml29@users.noreply.github.com>
2024-01-26 15:09:33 -0300

    breaking records

---
+++ breakingRecords.js
@@ -0,0 +1,21 @@
+function breakingRecords(scores) {
+   let min = scores[0];
+   let max = scores[0];
+   let minRecord = 0;
+   let maxRecord = 0; 
+   let result = [];
+   scores.forEach(score => {
+       if (score > max) {
+        maxRecord++;
+        max = score;
+       } else if (score < min) {
+        minRecord++;
+        min = score;
+       } 
+   });
+   result.push(maxRecord);
+   result.push(minRecord);
+   return result;
+}
+
+console.log(breakingRecords([12,24,10,24]));
