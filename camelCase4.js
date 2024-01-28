42ad02f4b5e05c8fb3064e5aa7149e41d9645d71
cristianml29 <156484062+cristianml29@users.noreply.github.com>
2024-01-28 04:19:58 -0300

    My solution is fine but hackerrank only accepts Java for this one.

--- camelCase4.js
+++ camelCase4.js
@@ -1,20 +1,78 @@
 function processData(input) {
-   let splittedInput = input.split("");
-   let result = [];
-   let finalResult = "";
-   if (splittedInput[0] === "S" && splittedInput[2] === "M") {
-    for (let i = 4; i < splittedInput.length; i++) {
-       if (splittedInput[i] === splittedInput[i].toUpperCase()) {
-            result.push(" ");
-            result.push(splittedInput[i].toLowerCase());
-       } else {
+    let rule = input.slice(0,3);
+    let splittedInput = input.split("");
+    let result = [];
+    let finalResult = "";
+
+    if (rule === "S;M") {
+        for (let i = 4; i < splittedInput.length; i++) {
+            if (splittedInput[i] === splittedInput[i].toUpperCase()) {
+                result.push(" ");
+                result.push(splittedInput[i].toLowerCase());
+            } else {
+                result.push(splittedInput[i]);
+            }
+        }
+        finalResult = result.join("");
+        console.log(finalResult.slice(0, -4));
+    } else if (rule === "C;V") {
+        for (let i = 4; i < splittedInput.length; i++) {
+            if (splittedInput[i-1] === " ") {
+                result.push(splittedInput[i].toUpperCase());
+            } else {
+                result.push(splittedInput[i]);
+            }
+        }
+        finalResult = result.join("");
+        console.log(finalResult.replaceAll(" ", ""));
+    } else if (rule === "C;C") {
+        result.push(splittedInput[4].toUpperCase());
+        for (let i = 5; i < splittedInput.length; i++) {
+            if (splittedInput[i-1] === " ") {
+                result.push(splittedInput[i].toUpperCase());
+            } else {
+                result.push(splittedInput[i]);
+            } 
+        }
+        finalResult = result.join("");
+        console.log(finalResult.replace(" ", ""));
+    } else if (rule === "S;C") {
+        result.push(splittedInput[4].toLowerCase());
+        for (let i = 5; i < splittedInput.length; i++) {
+            if (splittedInput[i] === splittedInput[i].toUpperCase()) {
+                result.push(" ");
+                result.push(splittedInput[i].toLowerCase());
+            } else {
+                result.push(splittedInput[i]);
+            }
+        }
+        finalResult = result.join("");
+        console.log(finalResult);
+    } else if (rule === "C;M") {
+        for (let i = 4; i < splittedInput.length; i++) {
+            if (splittedInput[i-1] === " ") {
+                result.push(splittedInput[i].toUpperCase());
+            } else {
+                result.push(splittedInput[i]);
+            }
+        }
+        finalResult = result.join("");
+        console.log(finalResult.replaceAll(" ", "") + "()");
+    } else if (rule === "S;V") {
+        result.push(splittedInput[4].toLowerCase());
+        for (let i = 5; i < splittedInput.length; i++) {
+           if (splittedInput[i] === splittedInput[i].toUpperCase()) {
+                result.push(" ");
+                result.push(splittedInput[i].toLowerCase());
+           } else {
             result.push(splittedInput[i]);
-       } 
+           }
+        }
+        finalResult = result.join("");
+        console.log(finalResult);
     }
-    
-   }
-   finalResult = result.join("");
-   return finalResult.slice(-4); 
+    return;
 }
 
-console.log(processData("S;M;plasticCup()"));
+processData("S;C;OrangeHighlighter");
+
